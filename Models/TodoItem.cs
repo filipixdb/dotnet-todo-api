535c40e0084400dc9027716e4adb583d3f018a8a
Filipi Xavier <filipi.xdb@gmail.com>
2019-03-23 03:33:55 -0300

    mudando JsonProperty names, quebrou o frontzinho

--- Models/TodoItem.cs
+++ Models/TodoItem.cs
@@ -1,9 +1,14 @@
+using Newtonsoft.Json;
+
 namespace TodoApi.Models
 {
     public class TodoItem
     {
+        [JsonProperty(PropertyName = "id")]
         public long Id { get; set; }
+        [JsonProperty(PropertyName = "title")]
         public string Name { get; set; }
+        [JsonProperty(PropertyName = "complete")]
         public bool isComplete { get; set; }
     }
 }
